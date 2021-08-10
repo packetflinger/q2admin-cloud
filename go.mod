5af1a10c43079dc0952de2984378a0a5ca5e4503
Joe Reid <joe@joereid.com>
2021-08-10 07:33:08 -0400

    Just a start, concurrent tcp connections working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/packetflinger/q2admind
+
+go 1.16
---
+++ q2admind.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type MessageBuffer struct {
+	buffer []byte
+	index  int32
+	length int32 // maybe not needed
+}
+
+type Player struct {
+	clientid     int8
+	name         string
+	userinfo     string
+	frags        int16
+	deaths       int16
+	suicides     int16
+	teleports    int16
+	lastteleport int32
+}
+
+// this is a Quake 2 Gameserver, and also a client to us
+type Server struct {
+	id         int32 // this is the database index
+	key        int32
+	index      int32
+	version    int32 // what version are we running
+	ipaddress  string
+	port       int16 // default 27910
+	connected  bool
+	currentmap string
+	enabled    bool
+	connection *net.Conn
+	players    []Player
+	message    MessageBuffer
+}
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
+
+		result := strconv.Itoa(rand.Intn(100)) + "\n"
+		c.Write([]byte(string(result)))
+	}
+	c.Close()
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+
+	port := ":" + arguments[1]
+	listener, err := net.Listen("tcp", port) // v4 + v6
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer listener.Close()
+
+	rand.Seed(time.Now().Unix())
+
+	for {
+		c, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
+}
+
+func init() {
+
+}
