dbf8acaa9bf96fdaa03686e2537c5a0851ab6e53
Joe Reid <joe@joereid.com>
2021-08-10 14:07:38 -0400

    Alpha order...

--- crypto.go
+++ crypto.go
@@ -11,60 +11,6 @@ import (
     "os"
 )
 
-func LoadPrivateKey(keyfile string) (*rsa.PrivateKey, error) {
-    priv, err := os.ReadFile(keyfile)
-    privPem, _ := pem.Decode(priv)
-
-	if privPem.Type != "RSA PRIVATE KEY" {
-		return nil, errors.New("Not a private key file")
-	}
-
-    var parsedKey interface{}
-	if parsedKey, err = x509.ParsePKCS1PrivateKey(privPem.Bytes); err != nil {
-		if parsedKey, err = x509.ParsePKCS8PrivateKey(privPem.Bytes); err != nil { // note this returns type `interface{}`
-            return nil, errors.New("Unable to parse private key file")
-		}
-	}
-
-	var privateKey *rsa.PrivateKey
-	var ok bool
-	privateKey, ok = parsedKey.(*rsa.PrivateKey)
-
-    if ok {
-        return privateKey, nil
-    }
-
-    return nil, errors.New("Something went wrong")
-}
-
-func PublicEncrypt(key *rsa.PublicKey, plaintext []byte) []byte {
-    encryptedBytes, err := rsa.EncryptOAEP(
-    	sha256.New(),
-    	rand.Reader,
-    	key,
-    	plaintext,
-    	nil)
-
-    if err != nil {
-    	panic(err)
-    }
-
-    return encryptedBytes
-}
-
-func PrivateDecrypt(key *rsa.PrivateKey, ciphertext []byte) []byte {
-    plaintext, err := key.Decrypt(
-        nil,
-        ciphertext,
-        &rsa.OAEPOptions{Hash: 5})  // sha256 (https://pkg.go.dev/crypto#DecrypterOpts)
-    if err != nil {
-        fmt.Println(err)
-        return nil
-    }
-
-    return plaintext
-}
-
 /**
  * Generate a private/public key pair of a certain bit length
  */
@@ -116,4 +62,58 @@ func GenerateKeys(bitlength int) bool {
     }
 
     return true
+}
+
+func LoadPrivateKey(keyfile string) (*rsa.PrivateKey, error) {
+    priv, err := os.ReadFile(keyfile)
+    privPem, _ := pem.Decode(priv)
+
+	if privPem.Type != "RSA PRIVATE KEY" {
+		return nil, errors.New("Not a private key file")
+	}
+
+    var parsedKey interface{}
+	if parsedKey, err = x509.ParsePKCS1PrivateKey(privPem.Bytes); err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(privPem.Bytes); err != nil { // note this returns type `interface{}`
+            return nil, errors.New("Unable to parse private key file")
+		}
+	}
+
+	var privateKey *rsa.PrivateKey
+	var ok bool
+	privateKey, ok = parsedKey.(*rsa.PrivateKey)
+
+    if ok {
+        return privateKey, nil
+    }
+
+    return nil, errors.New("Something went wrong")
+}
+
+func PrivateDecrypt(key *rsa.PrivateKey, ciphertext []byte) []byte {
+    plaintext, err := key.Decrypt(
+        nil,
+        ciphertext,
+        &rsa.OAEPOptions{Hash: 5})  // sha256 (https://pkg.go.dev/crypto#DecrypterOpts)
+    if err != nil {
+        fmt.Println(err)
+        return nil
+    }
+
+    return plaintext
+}
+
+func PublicEncrypt(key *rsa.PublicKey, plaintext []byte) []byte {
+    encryptedBytes, err := rsa.EncryptOAEP(
+    	sha256.New(),
+    	rand.Reader,
+    	key,
+    	plaintext,
+    	nil)
+
+    if err != nil {
+    	panic(err)
+    }
+
+    return encryptedBytes
 }
