1220adb4729311a68a97c00e1a4e2c1d160302df
Joe Reid <joe@joereid.com>
2021-08-10 09:26:42 -0400

    Reading all types works (added ReadData)

--- q2admind.go
+++ q2admind.go
@@ -13,6 +13,8 @@ import (
     "bytes"
 )
 
+// use a custom buffer struct to keep track of where
+// we are in the stream of bytes internally
 type MessageBuffer struct {
 	buffer []byte
 	index  int32
@@ -95,55 +97,69 @@ func main() {
 
 func init() {
     data := []byte{0x60,0x05,0x00,0x00,0x0C,0x22,0x00,0x00,0x00,0xCD,0x62,0xF0,0x0A,0x01,0x6F,0x70,0x65,0x6E,0x74,0x64,0x6D,0x00}
-    //var msg MessageBuffer
-    //msg.buffer = data
-    //msg := new(MessageBuffer{buffer:data, index:0, length:len(data)})
-    fmt.Printf("Long value: %d\n", ReadLong(data[4:]))
-    fmt.Printf("Short value: %d\n", ReadShort(data[9:]))
-    fmt.Printf("Byte value: %d\n", ReadByte(data[11:]))
-    fmt.Printf("String value: %s\n", ReadString(data[14:]))
+    var msg MessageBuffer
+    msg.buffer = data
+
+    //fmt.Printf("Long value: %d\n", ReadLong(&msg))
+    //fmt.Printf("Long value: %d\n", ReadLong(&msg))
+    //fmt.Printf("Long value: %d\n", ReadLong(&msg))
+    //fmt.Printf("Short value: %d\n", ReadShort(&msg))
+    fmt.Println(ReadData(&msg, 14))
+    fmt.Printf("String value: %s\n", ReadString(&msg))
 
     os.Exit(1)
 }
 
-func ReadString(d []byte) string {
+// basically just grab a subsection of the buffer
+func ReadData(msg *MessageBuffer, length int32) []byte {
+    start := msg.index
+    msg.index += length
+    return msg.buffer[start:msg.index]
+}
+
+func ReadString(msg *MessageBuffer) string {
     var buffer bytes.Buffer
 
     // find the next null (terminates the string)
-    for i:=0; d[i]!=0; i++ {
-        buffer.WriteString(string(d[i]))
+    for i:=0; msg.buffer[msg.index]!=0; i++ {
+        buffer.WriteString(string(msg.buffer[msg.index]))
+        msg.index++
     }
 
     return buffer.String()
 }
 
-func ReadLong(d []byte) int32 {
+func ReadLong(msg *MessageBuffer) int32 {
     var tmp struct {
         Value int32
     }
 
-    r := bytes.NewReader(d)
+    r := bytes.NewReader(msg.buffer[msg.index:])
     if err := binary.Read(r, binary.LittleEndian, &tmp); err != nil {
         fmt.Println("binary.Read failed:", err)
     }
 
+    msg.index += 4
     return tmp.Value
 }
 
-func ReadShort(d []byte) int16 {
+func ReadShort(msg *MessageBuffer) int16 {
     var tmp struct {
         Value int16
     }
 
-    r := bytes.NewReader(d)
+    r := bytes.NewReader(msg.buffer[msg.index:])
     if err := binary.Read(r, binary.LittleEndian, &tmp); err != nil {
         fmt.Println("binary.Read failed:", err)
     }
 
+    msg.index += 2
     return tmp.Value
 }
 
 // for consistency
-func ReadByte(d []byte) byte {
-    return byte(d[0])
+func ReadByte(msg *MessageBuffer) byte {
+    val := byte(msg.buffer[msg.index])
+    msg.index++
+    return val
 }
