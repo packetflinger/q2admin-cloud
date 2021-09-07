29c660821910b1685ac9272b9c9c5a5dfd96794a
Joe Reid <joe@joereid.com>
2021-09-07 15:41:54 -0400

    client/server signature verification seems to work now.

--- q2admind.go
+++ q2admind.go
@@ -1,57 +1,69 @@
 package main
 
 import (
-    "bufio"
+    //"bufio"
+    "encoding/hex"
     "encoding/json"
+    "errors"
     "fmt"
     "log"
     "math/rand"
     "net"
     "os"
     "crypto/rsa"
-    "strconv"
-    "strings"
+    //"strconv"
+    //"strings"
     "time"
 )
+
+const versionRequired = 200
 
 // use a custom buffer struct to keep track of where
 // we are in the stream of bytes internally
 type MessageBuffer struct {
     buffer []byte
-    index  int32
-    length int32 // maybe not needed
+    index  int
+    length int // maybe not needed
 }
 
 type Player struct {
-    clientid     int8
+    clientid     int
     name         string
     userinfo     string
-    frags        int16
-    deaths       int16
-    suicides     int16
-    teleports    int16
-    lastteleport int32
+    frags        int
+    deaths       int
+    suicides     int
+    teleports    int
+    lastteleport int
 }
 
 // this is a Quake 2 Gameserver, and also a client to us
 type Server struct {
-    id         int32 // this is the database index
-    key        int32
-    index      int32
-    version    int32 // what version are we running
+    id         int // this is the database index
+    key        int
+    index      int
+    version    int // what version are we running
     name       string
     ipaddress  string
-    port       int16 // default 27910
+    port       int // default 27910
     connected  bool
     currentmap string
     enabled    bool
     connection *net.Conn
     players    []Player
     message    MessageBuffer
+    messageout MessageBuffer
     encrypted  bool
     publickey  *rsa.PublicKey
     aeskey     []byte          // 16 (128bit)
     nonce      []byte          // 12 for gcm
+    challenge  []byte
+}
+
+// "this" admin server
+type AdminServer struct {
+    privatekey  *rsa.PrivateKey
+    publickey   *rsa.PublicKey
 }
 
 // structure of the config file
@@ -59,35 +71,215 @@ type Config struct {
     Address string
     Port int
     Database   string
-    Privatekey string
+    PrivateKey string
     APIPort    int
 }
 
 var config Config
+var q2a AdminServer
 
-var Servers = []Server {
+const (
+    _ = iota
+    CMDHello
+    CMDQuit
+    CMDConnect
+    CMDDisconnect
+    CMDPlayerList
+    CMDPlayerUpdate
+    CMDPrint
+    CMDCommand
+    CMDPlayers
+    CMDFrag
+    CMDMap
+    CMDPing
+    CMDAuth
+)
+
+const (
+    _ = iota
+    SCMDHelloAck
+    SCMDError
+    SCMDPong
+    SCMDComand
+    SCMDSayClient
+    SCMDSayAll
+    SCMDAuth
+    SCMDTrusted
+    SCMDKey
+)
+
+var servers = []Server {
     {id: 1, key:1234, name: "dm", ipaddress: "107.174.230.210", port: 27910, enabled: true},
     {id: 2, key:2345, name: "dmx", ipaddress: "107.174.230.210", port: 27911, enabled: true},
     {id: 3, key:4567, name: "tourney", ipaddress: "107.174.230.210", port: 27912, enabled: true},
     {id: 4, key:5678, name: "tourney2", ipaddress: "107.174.230.210", port: 27913, enabled: true},
 }
 
-func handleConnection(c net.Conn) {
-    fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-    for {
-        netData, err := bufio.NewReader(c).ReadString('\n')
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
+func clearmsg(msg *MessageBuffer) {
+    msg.buffer = []byte{0x00}
+    msg.index = 0
+    msg.length = 0
+}
 
-        temp := strings.TrimSpace(string(netData))
-        if temp == "STOP" {
+func parseMessage(msg *MessageBuffer) {
+    /*
+    for {
+        if msg.index >= len(msg.buffer) {
             break
         }
 
-        result := strconv.Itoa(rand.Intn(100)) + "\n"
-        c.Write([]byte(string(result)))
+        switch b := ReadByte(msg); b {
+        case CMDHello:
+            //parseHello(msg)
+        }
+    }
+    */
+}
+
+/**
+ * Locate the struct of the server for a particular
+ * ID, get a pointer to it
+ */
+func findserver(lookup int) (*Server, error) {
+    for _, srv := range(servers) {
+        if srv.key == lookup {
+            return &srv, nil
+        }
+    }
+
+    return nil, errors.New("Unknown server")
+}
+
+/**
+ * Setup the connection
+ * The first message sent should identify the game server
+ * and trigger the authentication process
+ */
+func handleConnection(c net.Conn) {
+    fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+
+    input := make([]byte, 5000)
+    var msg MessageBuffer
+
+    bytesread, _ := c.Read(input)
+    msg.buffer = input
+
+    fmt.Printf("Read Input:\n%s\n\n", hex.Dump(input[0:bytesread]))
+
+    magic := ReadLong(&msg)
+    if magic != 1128346193 {
+        // not a valid client, just close connection
+        c.Close()
+        return
+    }
+    log.Println("Magic value accepted")
+
+    _ = ReadByte(&msg) // should be CMDHello
+    key := ReadLong(&msg)
+    ver := ReadLong(&msg)
+    port := ReadShort(&msg)
+    _ = ReadByte(&msg) // max players
+    enc := ReadByte(&msg)
+    nonce := ReadData(&msg, 16)
+
+    if ver < versionRequired {
+        // write an error, close, return
+        c.Close()
+        return
+    }
+    log.Println("Running acceptable version")
+
+    server, err := findserver(int(key))
+    if err != nil {
+        // write an error, close socket, returns
+        log.Println(err)
+        c.Close()
+        return
+    }
+    log.Printf("Server located: %s\n", server.name)
+
+    server.port = int(port)
+    server.encrypted = int(enc) == 1    // stupid bool conversion
+    server.nonce = nonce
+    server.connection = &c
+    server.connected = true
+    keyname := fmt.Sprintf("keys/%d.pem", key)
+
+    log.Printf("Trying to load public key: %s\n", keyname)
+    pubkey, err := LoadPublicKey(keyname)
+    server.publickey = pubkey
+    if err != nil {
+        log.Printf("Loading public key: %s\n", err.Error())
+    }
+
+    // no, we have to sign this data instead
+    //challengeCipher := PublicEncrypt(server.publickey, server.nonce)
+    //fmt.Printf("Client Nonce:\n%s\n\n", hex.Dump(server.nonce))
+    challengeCipher := Sign(q2a.privatekey, server.nonce)
+    WriteByte(SCMDHelloAck, &server.messageout)
+    WriteShort(len(challengeCipher), &server.messageout)
+    WriteData(challengeCipher, &server.messageout)
+
+    //fmt.Printf("Sending:\n%s\n\n", hex.Dump(server.messageout.buffer))
+
+    /**b
+     * if client requests encrypted transit, encrypt the session key/iv
+     * with the client's public key to keep it confidential
+     */
+    if server.encrypted {
+        aeskey := RandomBytes(16)
+        aesiv := RandomBytes(16)
+        blob := append(aeskey, aesiv...)
+        aescipher := PublicEncrypt(server.publickey, blob)
+        WriteData(aescipher, &server.messageout)
+    }
+
+    chal := RandomBytes(16)
+    server.challenge = chal
+    WriteData(server.challenge, &server.messageout)
+
+    c.Write(server.messageout.buffer)
+
+    // read the client signature
+    size, _ := c.Read(input)
+    msg.buffer = input
+    msg.index = 0
+    msg.length = size
+
+    op := ReadByte(&msg)    // should be CMDAuth (0x0d)
+    if op != CMDAuth {
+        c.Close()
+        return
+    }
+
+    sigsize := ReadShort(&msg)
+    clientSignature := ReadData(&msg, int(sigsize))
+    verified := VerifySignature(server.publickey, server.challenge, clientSignature)
+
+    if verified {
+        fmt.Printf("%s signature verified\n", server.name)
+    } else {
+        fmt.Printf("%s signature verifcation failed...", server.name)
+        c.Close()
+        return
+    }
+
+    for {
+        size, err := c.Read(input)
+        if err != nil {
+            log.Printf(
+                "%s disconnected: %s\n",
+                c.RemoteAddr().String(),
+                err.Error())
+            break;
+        }
+
+        msg.buffer = input
+        msg.index = 0
+        msg.length = size
+
+        fmt.Printf("Read:\n%s\n\n", hex.Dump(input[0:size]))
+        //parseMessage(&msgbuf)
     }
     c.Close()
 }
@@ -112,6 +304,23 @@ func main() {
 }
 
 func init() {
+    /*
+    priv, err := LoadPrivateKey("private-1628817495.pem")
+    pub, err := LoadPublicKey("public-1628817495.pem")
+    clear := RandomBytes(16)
+    sig := Sign(priv, clear)
+    ok := VerifySignature(pub, clear, sig)
+    fmt.Printf("%s\n\n", hex.Dump(clear))
+    fmt.Printf("%s\n", hex.Dump(sig))
+    fmt.Println(ok)
+    os.Exit(1)
+    */
+
+    //pubkey, err := LoadPublicKey("public-1628712203.pem")
+    //pubkey, err := LoadPublicKey("private.pem")
+    //fmt.Println(pubkey)
+    //os.Exit(1)
+
     configfile := "q2a.json" // override with cli arg
     if len(os.Args) > 1 {
         configfile = os.Args[1]
@@ -128,16 +337,9 @@ func init() {
     }
 
     rand.Seed(time.Now().Unix())
-    // testing stuffb
-    //public, err := LoadPublicKey("public.pem")
-    //if err != nil {
-    //    fmt.Println(err)
-    //}
-    //fmt.Println(public)
-    //os.Exit(1)
-    /*
-    for _, srv := range(Servers) {
-        fmt.Printf("%d - %s - %s:%d\n", srv.id, srv.name, srv.ipaddress, srv.port)
-    }
-    */
+
+    privkey, _ := LoadPrivateKey(config.PrivateKey)
+    pubkey := privkey.Public().(*rsa.PublicKey)
+    q2a.privatekey = privkey
+    q2a.publickey = pubkey
 }
