9fc26f876f84281a620e5a90a744410adf6ee687
Joe Reid <joe@joereid.com>
2021-09-20 18:42:29 -0400

    Move commands to separate file

--- q2admind.go
+++ q2admind.go
@@ -206,55 +206,6 @@ func UserinfoMap(ui string) (map[string]string) {
     return info
 }
 
-
-
-func Teleport(srv *Server) {
-    cl := ReadByte(&srv.message)
-    dest := ReadString(&srv.message)
-    p := findplayer(srv.players, int(cl))
-    log.Printf("[%s/TELEPORT/%s] %s\n", srv.name, p.name, dest)
-
-    txt := "Sorry, teleport command is still under construction\n"
-    SayPlayer(srv, int(cl), PRINT_HIGH, txt)
-}
-
-func Invite(srv *Server) {
-    cl := ReadByte(&srv.message)
-    text := ReadString(&srv.message)
-    p := findplayer(srv.players, int(cl))
-    log.Printf("[%s/INVITE/%s] %s\n", srv.name, p.name, text)
-
-    //txt := "Sorry, INVITE command is currently under construction\n"
-    //SayPlayer(srv, int(cl), PRINT_HIGH, txt)
-    //StuffPlayer(srv, int(cl), "say this better work")
-    MutePlayer(srv, p.clientid, 15)
-}
-
-/**
- * Force a player to do a command
- */
-func StuffPlayer(srv *Server, cl int, cmd string) {
-    stuffcmd := fmt.Sprintf("sv !stuff CL %d %s\n", cl, cmd)
-    WriteByte(SCMDCommand, &srv.messageout)
-    WriteString(stuffcmd, &srv.messageout)
-}
-
-/**
- * Temporarily prevent the player from talking
- * using a negative number of seconds makes it
- * permanent.
- */
-func MutePlayer(srv *Server, cl int, seconds int) {
-    cmd := ""
-    if seconds < 0 {
-        cmd = fmt.Sprintf("sv !mute CL %d PERM\n", cl)
-    } else {
-        cmd = fmt.Sprintf("sv !mute CL %d %d", cl, seconds)
-    }
-    WriteByte(SCMDCommand, &srv.messageout)
-    WriteString(cmd, &srv.messageout)
-}
-
 /**
  * Locate the struct of the server for a particular
  * ID, get a pointer to it
