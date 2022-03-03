fe9ab75445e027a534ff67e55fa89ac3ea8a39af
Joe Reid <joe@joereid.com>
2022-03-02 21:31:10 -0500

    Switch from mysql to sqlite

--- database.go
+++ database.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
-    "log"
+	"database/sql"
+	"log"
+
+	//_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func DatabaseConnect() *sql.DB {
-    db, err := sql.Open("mysql", config.DBString)
-    if err != nil {
-        panic(err)
-    }
+	db, err := sql.Open("sqlite3", config.Database)
+	if err != nil {
+		panic(err)
+	}
 
-    if err = db.Ping(); err != nil {
-        panic(err)
-    }
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
 
-    return db
+	return db
 }
 
 /**
@@ -24,29 +26,29 @@ func DatabaseConnect() *sql.DB {
  * into a structure
  */
 func LoadServers(db *sql.DB) []Server {
-    sql := "SELECT id, uuid, owner, name, ip, port, disabled FROM server"
-    r, err := db.Query(sql)
-    if err != nil {
-        panic(err)
-    }
-    var srvs []Server
-    var srv Server
-    var disabled int
-    for r.Next() {
-        r.Scan(&srv.id, &srv.uuid, &srv.owner, &srv.name, &srv.ipaddress, &srv.port, &disabled)
-        srv.enabled = disabled == 0
-        srvs = append(srvs, srv)
-    }
-    r.Close()
+	sql := "SELECT id, uuid, owner, name, ip, port, disabled FROM server"
+	r, err := db.Query(sql)
+	if err != nil {
+		panic(err)
+	}
+	var srvs []Server
+	var srv Server
+	var disabled int
+	for r.Next() {
+		r.Scan(&srv.id, &srv.uuid, &srv.owner, &srv.name, &srv.ipaddress, &srv.port, &disabled)
+		srv.enabled = disabled == 0
+		srvs = append(srvs, srv)
+	}
+	r.Close()
 
-    return srvs
+	return srvs
 }
 
 func LogEventToDatabase(server int, logtype int, logentry string) {
-    sql := "INSERT INTO logdata (server, msgtype, entry, entrydate) VALUES (?,?,?,NOW())"
-    q, err := db.Query(sql, server, logtype, logentry)
-    if err != nil {
-        log.Println(err)
-    }
-    q.Close()
+	sql := "INSERT INTO logdata (server, msgtype, entry, entrydate) VALUES (?,?,?,NOW())"
+	q, err := db.Query(sql, server, logtype, logentry)
+	if err != nil {
+		log.Println(err)
+	}
+	q.Close()
 }
