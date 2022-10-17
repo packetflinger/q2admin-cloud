4bff5fc209d528fd85f1768727b12a83bce1de6c
Joe Reid <joe@joereid.com>
2022-10-17 18:10:44 +0000

    Not sure where to put certain funcs

---
+++ utils.go
@@ -0,0 +1,49 @@
+package main
+
+import "log"
+
+//
+// Someone deleted a managed server via the web interface.
+// This should mean:
+// - remove from database, including foreign key constraints
+// - close any open connections to this server
+// - remove from active server slice in memory
+//
+func RemoveServer(uuid string) bool {
+	srv, err := findserver(uuid)
+	if err != nil {
+		return false
+	}
+
+	// mark in-ram server object as disabled to prevent reconnects
+	srv.Enabled = false
+
+	// close any connections
+	//if srv.Connected {
+	//}
+
+	tr, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	sql := "DELETE FROM server WHERE id = ? LIMIT 1"
+	_, err = tr.Exec(sql, srv.ID)
+	if err != nil {
+		log.Println(err)
+		tr.Rollback()
+		return false
+	}
+	/*
+		sql = "DELETE FROM logdata WHERE server = ? LIMIT 1"
+		_, err = tr.Exec(sql, srv.ID)
+		if err != nil {
+			log.Println(err)
+			tr.Rollback()
+			return false
+		}
+	*/
+	tr.Commit()
+	return true
+}
