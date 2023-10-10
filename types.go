d7b3b677589581f96d91e13eeb134259bb7b62d4
Joe Reid <joe@joereid.com>
2023-10-09 22:18:46 -0400

    Remove old config type

--- types.go
+++ types.go
@@ -12,28 +12,6 @@ type MessageBuffer struct {
 	length int // maybe not needed
 }
 
-/*
-//
-// The config file once parsed
-//
-type Config struct {
-	Address         string `json:"address"`
-	Port            int    `json:"port"`
-	Database        string `json:"database"`
-	DBString        string `json:"dbstring"`
-	PrivateKey      string `json:"privatekey"`
-	APIPort         int    `json:"apiport"`
-	Debug           int    `json:"debug"`
-	APIEnabled      int    `json:"enableapi"`
-	ClientsFile     string `json:"clientsfile"`
-	ClientDirectory string `json:"clientdir"`  // folder for json files
-	UsersFile       string `json:"usersfile"`  // web users
-	AccessFile      string `json:"accessfile"` // their permissions
-	OAuthFile       string `json:"oauthfile"`  // api credentials
-	MaintenanceTime int    `json:"mainttime"`  // seconds to sleep
-}
-*/
-
 // Websocket
 type WebSocketConnection struct {
 	Connected bool
