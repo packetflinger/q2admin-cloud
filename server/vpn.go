d54160677f7916c591cc4ec45d6dd4c01dedc9b9
Joe Reid <joe@joereid.com>
2024-02-11 22:24:08 -0500

    Basic VPN detection and tests

---
+++ server/vpn.go
@@ -0,0 +1,107 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	pb "github.com/packetflinger/q2admind/proto"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+var (
+	VPNCache map[string]VPNCacheEntry
+)
+
+// Address ranges associated with VPN providers will probably
+// not update very frequently. If an IP is not in the cache,
+// look it up from the upstream source, then save it for future
+// lookups.
+type VPNCacheEntry struct {
+	ip      string // the IP in question
+	vpn     bool   // is this IP from a VPN provider?
+	ttl     int64  // unix timestamp when this record is considered stale
+	lookups int64  // how many times this IP has been looked up
+}
+
+// Load the vpn config proto from disk
+func ReadVPNConfig(cfgfile string) (*pb.VPNConfig, error) {
+	var cfg *pb.VPNConfig
+	contents, err := os.ReadFile(cfgfile)
+	if err != nil {
+		return nil, err
+	}
+	err = prototext.Unmarshal(contents, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func InitVPNCache() {
+	VPNCache = make(map[string]VPNCacheEntry)
+}
+
+// Check if the supplied IP address is from a VPN service. First look at the cache, then
+// lookup via the actual service (and add to the cache)
+func IsVPN(ip string, cache map[string]VPNCacheEntry, config *pb.VPNConfig) (bool, error) {
+	if !config.Enabled {
+		return false, nil
+	}
+	r, found := cache[ip]
+	if found {
+		fmt.Println(ip, "found!")
+		if r.vpn && r.ttl > time.Now().Unix() {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	} else {
+		fmt.Println(ip, "not found")
+		lookup, err := lookupVPNStatus(ip, config)
+		if err != nil {
+			return false, err
+		}
+		entry := VPNCacheEntry{
+			ip:      ip,
+			vpn:     lookup,
+			ttl:     time.Now().Unix() + (86400 * 30), // a month
+			lookups: 0,
+		}
+		cache[ip] = entry
+		return lookup, nil
+	}
+}
+
+func lookupVPNStatus(ip string, config *pb.VPNConfig) (bool, error) {
+	type Results struct {
+		Security struct {
+			VPN bool `JSON:"vpn"`
+		} `JSON:"security"`
+	}
+
+	apiClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+	url := fmt.Sprintf(config.GetLookupUrl(), ip, config.GetApiKey())
+	res, err := apiClient.Get(url)
+	if err != nil {
+		return false, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return false, err
+	}
+	var results Results
+	err = json.Unmarshal(body, &results)
+	if err != nil {
+		return false, err
+	}
+
+	return results.Security.VPN, nil
+}
---
+++ server/vpn_test.go
@@ -0,0 +1,96 @@
+package server
+
+import (
+	"testing"
+	"time"
+
+	pb "github.com/packetflinger/q2admind/proto"
+)
+
+func TestLookupVPNStatus(t *testing.T) {
+	tests := []struct {
+		desc   string
+		ip     string
+		config *pb.VPNConfig
+		want   bool
+	}{
+		{
+			// normally this test is true, but removing my api key...
+			desc: "test1",
+			ip:   "154.6.80.10",
+			config: &pb.VPNConfig{
+				Enabled:   true,
+				LookupUrl: "https://vpnapi.io/api/%s?key=%s",
+				ApiKey:    "yeahright",
+			},
+			want: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := lookupVPNStatus(tc.ip, tc.config)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != tc.want {
+				t.Error("lookupVPNStatus() =", got, ", want", tc.want)
+			}
+		})
+	}
+}
+
+func TestIsVPN(t *testing.T) {
+	tests := []struct {
+		desc   string
+		ip     string
+		cache  map[string]VPNCacheEntry
+		config *pb.VPNConfig
+		want   bool
+	}{
+		{
+			desc:   "test1",
+			ip:     "154.6.80.10",
+			config: &pb.VPNConfig{Enabled: true},
+			cache: map[string]VPNCacheEntry{
+				"154.6.80.10": {
+					vpn: true,
+					ttl: time.Now().Unix() + 100,
+				},
+				"8.8.8.8": {
+					vpn: false,
+					ttl: time.Now().Unix() + 100,
+				},
+			},
+			want: true,
+		},
+		{
+			desc: "test2",
+			ip:   "154.6.80.10",
+			config: &pb.VPNConfig{
+				Enabled:   true,
+				LookupUrl: "https://vpnapi.io/api/%s?key=%s",
+				ApiKey:    "yeahright",
+			},
+			cache: map[string]VPNCacheEntry{
+				"8.8.8.8": {
+					vpn: false,
+					ttl: time.Now().Unix() + 100,
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := IsVPN(tc.ip, tc.cache, tc.config)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != tc.want {
+				t.Error("IsVPN() =", got, ", want", tc.want)
+			}
+		})
+	}
+}
