8b7002a3b8f3ebc7dd770410e001ed7d7994f1f4
Joe Reid <joe@joereid.com>
2025-04-30 21:39:49 -0400

    Remove obvious comments

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// CloudAdmin is a centralized management service for Quake 2 game servers
+// running the q2admin game library. The game library will make a persistent
+// TCP connection to this service for logging and player management.
 package main
 
 import (
@@ -10,10 +13,9 @@ import (
 
 var (
 	config     = flag.String("config", "config/config.pb", "The main config file")
-	foreground = flag.Bool("foreground", false, "log to the console or file")
+	foreground = flag.Bool("foreground", false, "Log to the console or file")
 )
 
-// start here
 func main() {
 	flag.Parse()
 
@@ -26,6 +28,5 @@ func main() {
 		os.Exit(0)
 	}()
 
-	// run it
 	server.Startup(*config, *foreground)
 }
