1e40c87ced915103e2756e705ebe7ecd6fe8b033
Joe Reid <joe@joereid.com>
2025-06-04 21:26:12 -0400

    input checks

--- server/log.go
+++ server/log.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
@@ -14,6 +15,9 @@ import (
 //
 // Open the file and return a logger object for it.
 func NewClientLogger(cl *client.Client) (*log.Logger, error) {
+	if cl == nil {
+		return nil, fmt.Errorf("null client")
+	}
 	logfile := path.Join(srv.config.ClientDirectory, cl.Name, "log")
 	fp, err := os.OpenFile(logfile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
